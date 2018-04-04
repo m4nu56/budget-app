08e8472f3e4ee18909fb19f2d8ab52d928d3bee3
m4nu56 <m4nu56@gmail.com>
2018-04-04 21:55:39 +0100

    feat: integration coveralls in travis-ci

--- karma.conf.js
+++ karma.conf.js
@@ -16,8 +16,14 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
+      reports: [ 'html', 'lcovonly' ],
+      fixWebpackSourcePaths: true,
+      thresholds: {
+        statements: 80,
+        lines: 80,
+        branches: 80,
+        functions: 80
+      }
     },
     angularCli: {
       environment: 'dev'
