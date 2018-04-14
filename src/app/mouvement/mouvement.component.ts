dc8d8f79091b8413d2b2238f06ea8e31719b4c70
m4nu56 <m4nu56@gmail.com>
2018-04-14 09:45:59 +0100

    feat: remplacement de css par scss
    - rename des fichiers css en scss
    - changement du styleExt du projet angular dans angular-cli.json

--- src/app/mouvement/mouvement.component.ts
+++ src/app/mouvement/mouvement.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-mouvement',
   templateUrl: './mouvement.component.html',
-  styleUrls: ['./mouvement.component.css']
+  styleUrls: ['./mouvement.component.scss']
 })
 export class MouvementComponent implements OnInit {
 
